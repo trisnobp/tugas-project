abd9cbe6aeadd8223910a6f2e0df50b0ba8d97ce
trisnobp <trisnotyo6@gmail.com>
2022-09-11 22:02:44 +0700

    first commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_catalog,
+        'nama': 'Trisno Bayu Pamungkas',
+        'student_id': '2106702200'
+    }
+    return render(request, "katalog.html", context)
